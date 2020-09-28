dbcc2c8b2991d1e8c97babb93fc5edaa6f54c064
Naim Heera <abunaimheera@gmail.com>
2020-09-28 16:24:58 +0300

    Design aand responsiveness updated

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ function checkGuess() {
     finalResult.textContent = "Congratulations, you are a genius!"
     rating.textContent = "";
     guess.textContent = "";
-    setGameover();
+    setGameover1();
   } else if (guessCount === 10) {
-    finalResult.textContent = "You are retarded!"
-    finalResult.classList.add("retarded");
+    finalResult.textContent = "Oh no! You are better thah this!"
     guess.textContent = "";
     setGameover();
   } else {
@@ -50,6 +49,18 @@ function setGameover() {
   const resetParas = document.querySelector('.result p');
   resetButton = document.createElement("button");
   resetButton.textContent = "Try again!";
+  resetParas.appendChild(resetButton);
+  resetButton.classList.add("reset-class");
+  resetButton.addEventListener("click", resetGame);
+}
+
+function setGameover1() {
+  field.disabled = true;
+  submit.disabled = true;
+  rating.textContent = "";
+  const resetParas = document.querySelector('.result p');
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Play again!";
   resetParas.appendChild(resetButton);
   resetButton.classList.add("reset-class");
   resetButton.addEventListener("click", resetGame);
